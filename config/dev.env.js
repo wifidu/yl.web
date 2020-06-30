c07aa8cccf2ceaeb7e9f91be6c2fb3afb0f6efa8
wifidu <875147715@qq.com>
2020-07-01 00:33:07 +0800

    修改后端端口

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"http://59.110.212.116:32801/api"'
+  BASE_API: '"http://59.110.212.116:8090/api"'
 })
